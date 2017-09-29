6b062d3fc5cd7c9c204cc3978e1d25e619c8564f
LPJ <pingjunlin>
2017-09-29 18:04:24 +0800

    Add rongCloud Manager File

---
+++ KTVBariOS/Classes/ ThirdPartyManager/RongCloud/KTVRongCloudManager.m
@@ -0,0 +1,13 @@
+//
+//  KTVRongCloudManager.m
+//  KTVBariOS
+//
+//  Created by pingjun lin on 2017/9/29.
+//  Copyright © 2017年 Lin. All rights reserved.
+//
+
+#import "KTVRongCloudManager.h"
+
+@implementation KTVRongCloudManager
+
+@end
