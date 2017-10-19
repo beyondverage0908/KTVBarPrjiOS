0baa243a194321df4a328f5a35e0dc88da770a4a
LPJ <pingjunlin>
2017-10-19 14:13:50 +0800

    添加图片压缩了类

---
+++ KTVBariOS/Classes/Category/UIImage+extension.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+extension.h
+//  GongYunTong
+//
+//  Created by pingjun lin on 2017/5/22.
+//  Copyright © 2017年 vhs_health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (extension)
+
+- (UIImage *)resetSizeOfImageData:(UIImage *)source_image maxSize:(NSInteger)maxSize;
+
+@end
---
+++ KTVBariOS/Classes/Category/UIImage+extension.m
@@ -0,0 +1,117 @@
+//
+//  UIImage+extension.m
+//  GongYunTong
+//
+//  Created by pingjun lin on 2017/5/22.
+//  Copyright © 2017年 vhs_health. All rights reserved.
+//
+
+#import "UIImage+extension.h"
+
+@implementation UIImage (extension)
+
+- (UIImage *)resetSizeOfImageData:(UIImage *)source_image maxSize:(NSInteger)maxSize {
+    //先判断当前质量是否满足要求，不满足再进行压缩
+    __block NSData *finallImageData = UIImageJPEGRepresentation(source_image,1.0);
+    NSUInteger sizeOrigin   = finallImageData.length;
+    NSUInteger sizeOriginKB = sizeOrigin / 1024;
+    
+    if (sizeOriginKB <= maxSize) {
+        return source_image;
+    }
+    
+    //先调整分辨率
+    CGSize defaultSize = CGSizeMake(1024, 1024);
+    UIImage *newImage = [self newSizeImage:defaultSize image:source_image];
+    
+    finallImageData = UIImageJPEGRepresentation(newImage,1.0);
+    
+    //保存压缩系数
+    NSMutableArray *compressionQualityArr = [NSMutableArray array];
+    CGFloat avg   = 1.0/250;
+    CGFloat value = avg;
+    for (int i = 250; i >= 1; i--) {
+        value = i*avg;
+        [compressionQualityArr addObject:@(value)];
+    }
+    
+    /*
+     调整大小
+     说明：压缩系数数组compressionQualityArr是从大到小存储。
+     */
+    //思路：使用二分法搜索
+    finallImageData = [self halfFuntion:compressionQualityArr image:newImage sourceData:finallImageData maxSize:maxSize];
+    //如果还是未能压缩到指定大小，则进行降分辨率
+    while (finallImageData.length == 0) {
+        //每次降100分辨率
+        if (defaultSize.width-100 <= 0 || defaultSize.height-100 <= 0) {
+            break;
+        }
+        defaultSize = CGSizeMake(defaultSize.width-100, defaultSize.height-100);
+        UIImage *image = [self newSizeImage:defaultSize
+                                      image:[UIImage imageWithData:UIImageJPEGRepresentation(newImage,[[compressionQualityArr lastObject] floatValue])]];
+        finallImageData = [self halfFuntion:compressionQualityArr image:image sourceData:UIImageJPEGRepresentation(image,1.0) maxSize:maxSize];
+    }
+    
+    UIImage *finallImage = [UIImage imageWithData:finallImageData];
+    return finallImage;
+}
+
+#pragma mark 调整图片分辨率/尺寸（等比例缩放）
+
+- (UIImage *)newSizeImage:(CGSize)size image:(UIImage *)source_image {
+    CGSize newSize = CGSizeMake(source_image.size.width, source_image.size.height);
+    
+    CGFloat tempHeight = newSize.height / size.height;
+    CGFloat tempWidth = newSize.width / size.width;
+    
+    if (tempWidth > 1.0 && tempWidth > tempHeight) {
+        newSize = CGSizeMake(source_image.size.width / tempWidth, source_image.size.height / tempWidth);
+    }
+    else if (tempHeight > 1.0 && tempWidth < tempHeight){
+        newSize = CGSizeMake(source_image.size.width / tempHeight, source_image.size.height / tempHeight);
+    }
+    
+    UIGraphicsBeginImageContext(newSize);
+    [source_image drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+#pragma mark 二分法
+
+- (NSData *)halfFuntion:(NSArray *)arr image:(UIImage *)image sourceData:(NSData *)finallImageData maxSize:(NSInteger)maxSize {
+    NSData *tempData = [NSData data];
+    NSUInteger start = 0;
+    NSUInteger end = arr.count - 1;
+    NSUInteger index = 0;
+    
+    NSUInteger difference = NSIntegerMax;
+    while(start <= end) {
+        index = start + (end - start)/2;
+        
+        finallImageData = UIImageJPEGRepresentation(image,[arr[index] floatValue]);
+        
+        NSUInteger sizeOrigin = finallImageData.length;
+        NSUInteger sizeOriginKB = sizeOrigin / 1024;
+        CLog(@"当前降到的质量：%ld", (unsigned long)sizeOriginKB);
+        CLog(@"%lu----%lf", (unsigned long)index, [arr[index] floatValue]);
+        
+        if (sizeOriginKB > maxSize) {
+            start = index + 1;
+        } else if (sizeOriginKB < maxSize) {
+            if (maxSize-sizeOriginKB < difference) {
+                difference = maxSize-sizeOriginKB;
+                tempData = finallImageData;
+            }
+            end = index - 1;
+        } else {
+            break;
+        }
+    }
+    return tempData;
+}
+
+
+@end
